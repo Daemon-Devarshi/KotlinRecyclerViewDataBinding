e46eb6653f77f59deab4d4f700ba9f96204d7ddd
DevarshiK <devarshi.bluechip@gmail.com>
2017-10-04 20:19:10 +0530

    Binding code optimization.

--- app/src/main/java/daemonconstruction/recyclerbindingexample/MainActivity.kt
+++ app/src/main/java/daemonconstruction/recyclerbindingexample/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
         val personsAdapter = PersonsAdapter(PersonsProvider.getPersons())
         recyclerViewPersons.adapter = personsAdapter
         recyclerViewPersons.layoutManager = LinearLayoutManager(this)
--- app/src/main/java/daemonconstruction/recyclerbindingexample/PersonsAdapter.kt
+++ app/src/main/java/daemonconstruction/recyclerbindingexample/PersonsAdapter.kt
@@ -1,11 +1,11 @@
 package daemonconstruction.recyclerbindingexample
 
 import android.databinding.DataBindingUtil
-import android.databinding.ViewDataBinding
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import daemonconstruction.recyclerbindingexample.databinding.ListItemPersonBinding
 
 /**
  * Created by devarshi.k on 27/09/17.
@@ -27,8 +27,10 @@ class PersonsAdapter(private var persons: Array<Person>) : RecyclerView.Adapter<
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): PersonViewHolder {
         val itemView = LayoutInflater.from(parent!!.context).inflate(R.layout.list_item_person, parent, false)
-        return PersonViewHolder(itemView, DataBindingUtil.bind(itemView))
+        return PersonViewHolder(itemView)
     }
 
-    class PersonViewHolder(itemView: View, val binding: ViewDataBinding) : RecyclerView.ViewHolder(itemView) { }
+    class PersonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val binding: ListItemPersonBinding = ListItemPersonBinding.bind(itemView)
+    }
 }
